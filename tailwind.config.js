e9b98a67b722ba22a77d1fb5238042d48324948f
karol ariza <karol@rebootacademy.org>
2022-01-25 18:06:26 +0000

    header responsive

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    fontFamily: {
+      title: 'Staatliches, cursive',
+      parragraph: 'Lato, sans-serif'
+    },
+    colors: {
+      transparent: "transparent",
+       white: "#fff",
+      black: '#0c0c0c',
+      yellow: "#F5E828",
+      "light-yellow": "#EBE87B",
+      orange: "#FF9921",
+      "light-orange": "#EBDCA2",
+      lila: "#C790FF",
+      purple: "#8733F5",
+      error: "#ef4444",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
